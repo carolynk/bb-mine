fe24d3b04ee939263a539182d97059ebf449fff2
tagwaesam <tagwaesam5@gmail.com>
2020-05-08 22:49:21 -0400

    stil have problem of visted node sill visited again

--- mainCopy.py
+++ mainCopy.py
@@ -2,6 +2,7 @@ from graph import Node, Graph
 import unittest as ut
 import matplotlib.pyplot as plt
 
+from collections import defaultdict
 class BB:
 
     def __init__(self, g, m, s, e):
@@ -11,13 +12,19 @@ class BB:
         self.end_node = e
 
     # calculate cost
-    def calc_cost(self, v, path_from_start):
-        return 0
+    def calc_cost(self,path_from_start):
+        if path_from_start is not None:
+            return self.lowerBound(path_from_start[-1],path_from_start)
+        else:
+            return 0
 
     # select from F the (v,path_from_start) where the cost between s and v is the lowest
     def select_v_with_lowest_cost(self,f):
-        minCost =0
+        print(f)
+        minNode = None
+        minCost = -1
         for node in f.keys():
+            print(node)
             if minCost < f[node][1]:
                 minNode = node
 
@@ -26,17 +33,22 @@ class BB:
 
     # expand configuration
     def expand(self,node,f):
-        bathFromStart = f[node]
+        pathFromStart = f[node][0]
+        # print(pathFromStart)
         configration =[]
         for neighbour in self.graph[node].keys():
-            configration.append(list.copy(bathFromStart).append(neighbour))
+            print(neighbour)
+            temp = list.copy(pathFromStart)
+            temp.insert(0,node)
+            temp.append(neighbour)
+            configration.append(temp)
 
         return configration
 
     # return distance of edges in path
     def calculateDistane(self,path):
         distances =[]
-        for i in len(path):
+        for i in range(len(path)-1):
             distances.append(self.graph[path[i]][path[i+1]])
 
         return distances
@@ -47,45 +59,60 @@ class BB:
 
 
     def shortestPath(self):
-        F = {self.start_node:[[],0]} #contains {v:path_from_start,cost}
-        b = {}
-        visited = {} #contains {v:[path_from_start,cost]}
+        F = defaultdict(lambda :[]) #contains {v:path_from_start,cost}
+        F[self.start_node] = [[],0]
+        b = defaultdict(lambda :1)
+        visited = defaultdict(lambda :{}) #contains {v:[path_from_start,cost]}
         while F is not None:
             # select from F the (v,path_from_start) where the cost between s and v is the lowest
-            minv = self.select_v_with_lowest_cost(f)
+            minv = self.select_v_with_lowest_cost(F)
             # check if this node visited before or not
 
             # Expand
             configurations=self.expand(minv,F)
-            if minv not in visited[minv]:
+            print(configurations)
+            if minv not in visited.keys():
                 visited[minv]=[]
-                visited[minv].append(F[minv][0]) # add bathFromStart
+                visited[minv].append(F[minv][0]) # add pathFromStart
                 visited[minv].append(F[minv][1]) # add cost
 
-            for bathFromStart in configurations:
-                # calculate cost for v
-                c = self.calc_cost(bathFromStart)
-                lastNodeInBathFromStart = bathFromStart[-1]
-                if visited[lastNodeInBathFromStart][1] < c :
-                    check ="dead end"
-                elif lastNodeInBathFromStart is self.end_node and len(path_from_start) >= self.min_items:
-                    check = "solution found"
-                else:
-                    check = "continue"
-
-                if check is "solution found":
-                    if c < b[2]:
-                        b = {bathFromStart[-1], path_from_start, c}
+                print(visited)
+            else:
+                for pathFromStart in configurations:
+                    # calculate cost for v
+                    c = self.calc_cost(pathFromStart)
+                    # if pathFromStart is not None:
+                    lastNodeInpathFromStart = pathFromStart[-1]
+                    print(lastNodeInpathFromStart)
+                    print(visited[lastNodeInpathFromStart])
+                    if visited[lastNodeInpathFromStart] != {} and visited[lastNodeInpathFromStart][1] < c :
+                        check = "dead end"
+                    elif lastNodeInpathFromStart is self.end_node and len(path_from_start) >= self.min_items:
+                        check = "solution found"
                     else:
-                        # discard configuration
-                if check is "dead end":
-                        # discard configuration
+                        check = "continue"
 
-                else:
-                    if self.lowerBound(lastNodeInBathFromStart, path_from_start) <= b[2]:
-                        F[lastNodeInBathFromStart] = [path_from_start, c]
+                    if check == "solution found":
+                        if c < b[2]:
+                            b = {lastNodeInpathFromStart, pathFromStart, c}
+                        else:
+                            # discard configuration
+                            configurations.remove(pathFromStart)
+                            break
+                    elif check == "dead end":
+                            # discard configuration
+                            configurations.remove(pathFromStart)
+                            break
+
                     else:
-                        # discard configuration
+                        print(self.lowerBound(lastNodeInpathFromStart, pathFromStart))
+                        if self.lowerBound(lastNodeInpathFromStart, pathFromStart) <= b[2]:
+                            print("here")
+                            F[lastNodeInpathFromStart]= [pathFromStart, c]
+                            print("f",F)
+                        else:
+                            # discard configuration
+                            return
 
         return b
 
@@ -126,7 +153,7 @@ class BBTest(ut.TestCase):
     def test(self):
         pass
 
-def main:
+def main():
     graph = {"a": {"b":2,"c":1},
              "b": {"d":1},
              "c": {"a":1,"b":1,"d":3,"e":7},
